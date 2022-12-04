a04fc8013009fe5535de7ebd1f586efb2c942154
Cristian N Gonzalez <contact@cristianngonzalez.com>
2022-12-04 18:59:57 -0300

    Portfolio

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -27,29 +27,24 @@
                     </div>
                 </div><!-- item-->
             </div>
+
+
             <div class="swiper-slide wow fadeInUp">
-                <div class="slider-item ">
-                    <div class="overlay "></div>
-                    <div class="container">
-                        <div class="slider-inner style-2 home-1 flex">
-                            <div class="slider-content">
-                                <h1 class="heading">Frontend development</h1>
-                                <p class="sub-heading">Frontend development, we turn any sketch, photoshop or figma into an incredible website. We adapt it to the most popular frontend framework technologies.</p>
-                                <!--
-                                <div class="button-slider">
-                                    <a href="explore-1.html" class="sc-button btn-bordered-white style letter "><span>Explore More</span></a>
-                                    <a href="create-item.html" class="sc-button btn-bordered-white style file"><span>Create Now</span></a>
-                                </div>
-                                -->
-                            </div>
-                            <div class="slider-img">
-                                <div class="img-home-1"><img src="{{env('ASSETS_URL')}}assets/images/slider/frontenddev.png" alt="Image"></div>
-                            </div>
+                <div class="slider-item">
+                    <div class="overlay"></div>
+                    <div class="slider-inner flex home-1">
+                        <div class="slider-content">
+                            <h1 class="heading">Frontend development</h1>
+                            <p class="sub-heading">Frontend development, we turn any sketch, photoshop or figma into an incredible website. We adapt it to the most popular frontend framework technologies.</p>
+                        </div>
+                        <div class="slider-img">
+                            <div class="img-home-1"><img src="{{env('ASSETS_URL')}}assets/images/slider/frontenddev.png" alt="Image"></div>
                         </div>
                     </div>
-
                 </div><!-- item-->
             </div>
+
+
         </div>
         <div class="swiper-pagination"></div>
         <div class="swiper-button-next btn-slide-next "></div>
