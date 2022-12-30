10c5f7c506241a9d3c70389be7b714753249b1d4
Cristian Gonzalez <contact@cristianngonzalez.com>
2022-12-30 15:03:34 -0300

    Anlytics

--- resources/views/site/partials/header.blade.php
+++ resources/views/site/partials/header.blade.php
@@ -5,6 +5,15 @@
 <!--<![endif]-->
 
 <head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8B0MNXZD93"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-8B0MNXZD93');
+</script>
     <!-- Basic Page Needs -->
     <meta charset="utf-8">
 
