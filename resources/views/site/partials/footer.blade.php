60cf987756eba2936e7f0889b477479b287e7178
Cristian Gonzalez <contact@cristianngonzalez.com>
2024-02-03 19:19:30 -0300

    footer

--- resources/views/site/partials/footer.blade.php
+++ resources/views/site/partials/footer.blade.php
@@ -43,44 +43,16 @@
                     <div class="widget widget-logo">
                         <div class="logo-footer" id="logo-footer">
                             <a href="index.html">
-                                <img id="logo_footer" src="{{env('ASSETS_URL')}}assets/images/logo/logo_dark.png" alt="cristianngonzalez" width="151" height="45" data-retina="assets/images/logo/logo_dark@2x.png" data-width="151" data-height="45">
+                                
                             </a>
                         </div>
                         <p class="sub-widget-logo"></p>
                         
-                        <!------------------------------------------------------------------------------------------------------>
-                        <div class="col-12">
-                            <div class="row justify-content-md-start justify-content-center">
-                                <div class="col-auto">
-                                    <a href="https://www.facebook.com/cristianngonzalezok" class="social-link-footer" target="_blank" class="active"><i class="bi bi-facebook"></i></a>
-                                </div>
-                                <div class="col-auto">
-                                    <a href="https://github.com/cristianngonzalez/cristianngonzalez.com" class="social-link-footer" target="_blank" class="active"><i class="bi bi-github"></i></a>
-                                </div>
-                                <div class="col-auto">
-                                    <a href="https://www.linkedin.com/in/cristianngonzalez/" class="social-link-footer" target="_blank" class="active"><i class="bi bi-linkedin"></i></a>
-                                </div>
-                                <div class="col-auto">
-                                    <a href="https://www.youtube.com/channel/UCTfVTTAkvF7KF4CF7U64ORQ" class="social-link-footer" target="_blank" class="active"><i class="bi bi-youtube"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <!----------------------------------------------------------------------------------------------------->
+                        
 
                     </div>
                 </div>
                 
-            </div>
-            <div class="col-lg-2 col-md-6 col-sm-6 col-6">
-                <div class="widget widget-menu menu-marketplace text-md-left text-center">
-                    <h5 class="title-widget">Services</h5>
-                    <ul>
-                        <li><a href="#">Mobile apps</a></li>
-                        <li><a href="#">Web apps</a></li>
-                        <li><a href="#">Backend</a></li>
-                        <li><a href="#">Data Science</a></li>
-                    </ul>
-                </div>
             </div>
         </div>
     </div>
@@ -97,7 +69,7 @@
     <div class="container p-4">
       <div class="row">
         <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-          <h5>Cristian N Gonzalez</h5>
+          <span><img id="logo_footer" src="{{env('ASSETS_URL')}}assets/images/logo/logo_dark.png" alt="cristianngonzalez" width="151" height="45" data-retina="assets/images/logo/logo_dark@2x.png" data-width="151" data-height="45"></span>
           <p>
             Web and aplications development and dataminer.
           </p>
@@ -108,16 +80,16 @@
 
           <ul class="list-unstyled mb-0">
             <li>
-              <a href="#!" class="text-white">Enlace 1</a>
+              <a href="#!" class="text-white">About</a>
             </li>
             <li>
-              <a href="#!" class="text-white">Enlace 2</a>
+              <a href="#!" class="text-white">Contact</a>
             </li>
             <li>
-              <a href="#!" class="text-white">Enlace 3</a>
+              <a href="#!" class="text-white">Projects</a>
             </li>
             <li>
-              <a href="#!" class="text-white">Enlace 4</a>
+              <a href="#!" class="text-white">Courses</a>
             </li>
           </ul>
         </div>
@@ -127,16 +99,16 @@
 
           <ul class="list-unstyled">
             <li>
-              <a href="#!" class="text-white">Link 1</a>
+              <a href="https://www.linkedin.com/in/cristianngonzalez/" target="_blank" class="text-white"><i class="bi bi-linkedin"></i> Linkedin</a>
             </li>
             <li>
-              <a href="#!" class="text-white">Link 2</a>
+              <a href="https://www.youtube.com/channel/UCTfVTTAkvF7KF4CF7U64ORQ" class="text-white"><i class="bi bi-youtube"></i> YouTube</a>
             </li>
             <li>
-              <a href="#!" class="text-white">Link 3</a>
+              <a href="https://github.com/cristianngonzalez/cristianngonzalez.com" target="_blank" class="text-white"><i class="bi bi-github"></i> Github</a>
             </li>
             <li>
-              <a href="#!" class="text-white">Link 4</a>
+              <a href="https://www.facebook.com/cristianngonzalezok" target="_blank" class="text-white"><i class="bi bi-facebook"></i> Facebook</a>
             </li>
           </ul>
         </div>
@@ -145,7 +117,7 @@
     
     <!-- Derechos de autor -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2024 Derechos Reservados:
-      <a class="text-white" href="https://tusitio.com/">tusitio.com</a>
+      © {{date('Y')}} Derechos Reservados:
+      <a class="text-white" href="{{url('/')}}">www.cristianngonzalez.com</a>
     </div>
   </footer>
