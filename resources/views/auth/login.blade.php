a0c57016d41b7207caf1f23626307761edaf4f62
Cristian Gonzalez <contact@cristianngonzalez.com>
2024-02-03 22:32:51 -0300

    formulario de registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,82 +4,66 @@
 @section('content')
 
 
-<section class="tf-section login-page">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="form-create-item-content">
-                    <div class="form-create-item">
-                        <div class="sc-heading">
-                            <div class="col-12">
-                                <h3>{{ __('Login') }}</h3>
-                            </div>
-                        </div>
-                        <form id="create-item-1" class="form-container" method="POST" action="{{ route('login') }}">
-                            @csrf
-                            
-                            <div class="col-12 mt-3">
-                                <label for="email" >{{ __('Email Address') }}</label>      
-                                <input id="email" type="email" class="tb-my-input @error('email') is-invalid @enderror" name="email" aria-required="true" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-
-                            
-                            <div class="col-12 mt-3">
-                                <label for="password">{{ __('Password') }}</label>
-                                <!--Input password and button-eye should to have the same 
-                                    number on the end of its id, beacause it uses for jquery-->
-                                <input type="password" id="input-password-1" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                                <button class="btn btn-password-eye" type="button" id="btn-password-eye-1">
-                                    <i id="bi-password-eye-1" class="bi bi-eye"></i>
-                                </button>
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        
-                            <div class="col-12 mt-3">
-                                <div class="btn-check">
-                                    <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                    <label class="form-check-label" for="remember">{{ __('Remember Me') }}</label>
-                                </div>
-                            </div>
-
-                            <div class="col-12 mt-3">
-                                <button type="submit" class="sc-button style letter style-2">
-                                    {{ __('Login') }}
-                                </button>
-                            </div>
-
-                            <div class="col-6 mt-3">
-                                @if (Route::has('password.request'))
-                                    <a href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
-                                @endif
-                            </div>
-
-                            <div class="col-6 mt-3">
-                                <span><a href="{{route('register')}}">I am new here, register you here.</a></span>
-                            </div>
-
-
-                        </form>
-                    </div>
-                    <div class="form-background">
-                        <img src="{{env('ASSETS_URL')}}assets/images/background/img-login.jpg" alt="">
-                    </div>
-                </div>
+<div class="container-fluid">
+    <div class="row mt-5 mb-5 justify-content-center">
+        <div class="col-md-6 p-4 rounded-5 bg-warning">
+            <div class="sc-heading">
+                <h1 class="text-center">{{ __('Login') }}</h1>
             </div>
+
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                @method('POST')
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">{{ __('Email Address') }}</label>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" aria-required="true" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+
+                    <label class="form-label" for="password">{{ __('Password') }}</label>
+                    <!--Input password and button-eye should to have the same 
+                        number on the end of its id, beacause it uses for jquery-->
+                    <input type="password" id="input-password-1" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    <button class="btn btn-password-eye" type="button" id="btn-password-eye-1">
+                        <i id="bi-password-eye-1" class="bi bi-eye"></i>
+                    </button>
+
+                    @error('password')
+                        <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                    @enderror
+
+                </div>
+                <div class="mb-3 form-check">
+
+                        <input type="checkbox" class="form-check-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label class="form-check-label" for="remember">{{ __('Remember Me') }}</label>
+                   
+
+                </div>
+                <button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
+            </form>
+
+            <!-- La funcionalidad de recuperar contraseña todavia no existe
+            <div class="col-6 mt-3">
+                @if (Route::has('password.request'))
+                    <a href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+                @endif
+            </div>
+            -->
+
+            <div class="col-6 mt-3">
+                <span><a href="{{route('register')}}">I am new here, register you here.</a></span>
+            </div>
+
         </div>
     </div>
-</section>
-
+</div>
 
 
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,80 +3,79 @@
 
 @section('content')
 
-<section class="tf-section login-page">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="form-create-item-content">
-                    <div class="form-create-item">
-                        <div class="sc-heading">
-                            <div class="col-12">
-                                <h3>{{ __('Register') }}</h3>
-                            </div>
-                        </div>
-                        <form id="create-item-1" class="form-container" method="POST" action="{{ route('register') }}">
-                            @csrf
-                            
-                            <div class="col-12 mt-3">
-                                <label for="name" >{{ __('Name') }}</label>      
-                                <input id="name" type="text" class="@error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
 
-                            <div class="col-12 mt-3">
-                                <label for="email">{{ __('Email Address') }}</label>
-                                <input id="email" type="email" class=" @error('email') is-invalid @enderror" value="{{ old('email') }}" name="email" required autocomplete="email">
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
 
-                            <div class="col-12 mt-3">
-                                <label for="password">{{ __('Password') }}</label>
-                                <input id="input-password-1" type="password" class="@error('password') is-invalid @enderror" value="{{ old('email') }}" name="password" required autocomplete="new-password">
-                                <button class="btn btn-password-eye" type="button" id="btn-password-eye-1">
-                                    <i id="bi-password-eye-1" class="bi bi-eye"></i>
-                                </button>
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        
-                            <div class="col-12 mt-3">
-                                <label for="password-confirm">{{ __('Confirm Password') }}</label>
-                                <input id="input-password-2" type="password" class="@error('password') is-invalid @enderror" value="{{ old('email') }}" name="password_confirmation" required autocomplete="new-password">
-                                <button class="btn btn-password-eye" type="button" id="btn-password-eye-2">
-                                    <i id="bi-password-eye-2" class="bi bi-eye"></i>
-                                </button>
-                            </div>
-
-                            <div class="col-12 mt-3">
-                                <button type="submit" class="sc-button style letter style-2">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-
-                            <div class="col-12 mt-3">
-                                <span><a href="{{route('login')}}">I have an account, login here.</a></span>
-                            </div>
-
-                        </form>
-                    </div>
-                    <div class="form-background">
-                        <img src="{{env('ASSETS_URL')}}assets/images/background/img-login.jpg" alt="">
-                    </div>
-                </div>
+<div class="container-fluid">
+    <div class="row mt-5 mb-5 justify-content-center">
+        <div class="col-md-6 p-4 rounded-5 bg-warning">
+            <div class="sc-heading">
+                <h1 class="text-center">{{ __('Register') }}</h1>
             </div>
+
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+                @method('POST')
+
+                <div class="mb-3">
+                    <label for="name" class="form-label">{{ __('Name') }}</label>      
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+
+
+                    <label for="email" class="form-label">{{ __('Email Address') }}</label>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" name="email" required autocomplete="email">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label" for="password">{{ __('Password') }}</label>
+                    <input id="input-password-1" type="password" class="form-control @error('password') is-invalid @enderror" value="{{ old('email') }}" name="password" required autocomplete="new-password">
+                    <button class="btn btn-password-eye" type="button" id="btn-password-eye-1">
+                        <i id="bi-password-eye-1" class="bi bi-eye"></i>
+                    </button>
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+
+                <div class="mb-3">
+                        <label class="form-label" for="password-confirm">{{ __('Confirm Password') }}</label>
+                        <input id="input-password-2" type="password" class="form-control @error('password') is-invalid @enderror" value="{{ old('email') }}" name="password_confirmation" required autocomplete="new-password">
+                        <button class="btn btn-password-eye" type="button" id="btn-password-eye-2">
+                            <i id="bi-password-eye-2" class="bi bi-eye"></i>
+                        </button>
+                 
+                </div>
+
+
+                <button type="submit" class="btn btn-primary">{{ __('Register') }}</button>
+
+                
+
+            </form>
+
+
+            <div class="col-12 mt-3">
+                <span><a href="{{route('login')}}">I have an account, login here.</a></span>
+            </div>
+
         </div>
     </div>
-</section>
+</div>
+
 
 @endsection
