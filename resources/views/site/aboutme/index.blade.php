6002afe9284d59e25c5cbcff626a0970cefedc0d
Cristian Gonzalez <contact@cristianngonzalez.com>
2024-02-04 21:40:03 -0300

    revisando

--- resources/views/site/aboutme/index.blade.php
+++ resources/views/site/aboutme/index.blade.php
@@ -51,194 +51,89 @@
 
         <div class="col-md-3">
             <div class="card">
-                <img src="" alt="" class="card-img-top">
+                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-3.svg" alt="" class="card-img-top">
                 <div class="card-body">
-
+                    <h3 class="card-title">Vue</h3>
+                    <p class="card-text">Vue is a JavaScript frontend framework for building complex applications in a simpler and more scalable way.</p>
                 </div>
-                <p class="card-text"></p>
+            </div>
+        </div>
+
+      
+
+        <div class="col-md-3">
+            <div class="card">
+                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-4.svg" alt="" class="card-img-top">
+                <div class="card-body">
+                    <h3 class="card-title">WebGL & Unity</h3>
+                    <p class="card-text">Unity is the most popular graphic engine, this engine allows you to create very powerful graphic applications and export them to the web and other devices.</p>
+                </div>
             </div>
         </div>
 
         <div class="col-md-3">
             <div class="card">
-                <img src="" alt="" class="card-img-top">
+                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-5.svg" alt="" class="card-img-top">
                 <div class="card-body">
-
+                    <h3 class="card-title">Laravel</h3>
+                    <p class="card-text">Laravel is a Php framework for building backends for web applications. Laravel is based on the Symphony framework.</p>
                 </div>
-                <p class="card-text"></p>
-            </div>
-        </div>
-
-        <div class="col-md-3">
-            <div class="card">
-                <img src="" alt="" class="card-img-top">
-                <div class="card-body">
-
-                </div>
-                <p class="card-text"></p>
-            </div>
-        </div>
-
-        <div class="col-md-3">
-            <div class="card">
-                <img src="" alt="" class="card-img-top">
-                <div class="card-body">
-
-                </div>
-                <p class="card-text"></p>
             </div>
         </div>
 
 
         <div class="col-md-3">
             <div class="card">
-                <img src="" alt="" class="card-img-top">
+                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-6.svg" alt="" class="card-img-top">
                 <div class="card-body">
-
+                    <h3 class="card-title">NodeJS</h3>
+                    <p class="card-text">Nodejs is a JavaScript runtime built on Chrome's V8 JavaScript engine. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient.</p>
                 </div>
-                <p class="card-text"></p>
             </div>
         </div>
 
         <div class="col-md-3">
             <div class="card">
-                <img src="" alt="" class="card-img-top">
+                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-7.svg" alt="" class="card-img-top">
                 <div class="card-body">
-
+                    <h3 class="card-title">Wordpress</h3>
+                    <p class="card-text">Wordpress is the most popular cms, the vast majority of the world's websites use it. It has extensive documentation and tools created by third parties.</p>
                 </div>
-                <p class="card-text"></p>
             </div>
         </div>
+
+     
+                    
 
         <div class="col-md-3">
             <div class="card">
-                <img src="" alt="" class="card-img-top">
+                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-8.svg" alt="" class="card-img-top">
                 <div class="card-body">
-
+                    <h3 class="card-title">AdobeXD, Figma and Zeplin</h3>
+                    <p class="card-text">Adobe xd and figma are used to create and design application sketches, I am not a designer but in this time I have had to work with this.</p>
                 </div>
-                <p class="card-text"></p>
             </div>
         </div>
+
+    
+     
 
         <div class="col-md-3">
             <div class="card">
-                <img src="" alt="" class="card-img-top">
+                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-9.svg" alt="" class="card-img-top">
                 <div class="card-body">
-
+                    <h3 class="card-title">Electron & Ionic</h3>
+                    <p class="card-text">Ionic builds hybrid apps compatible with IOS, Android, Windows and Web. Ionic uses web standards (html, css, JavaScript)</p>
                 </div>
-                <p class="card-text"></p>
             </div>
         </div>
+
 
     </div>
 
 </div>
 
 
-
-
-
-    <div class="container">
-
-
-
-        <!----------------------------------------------------------------------------------------------------------------------------------------->
-        <!--Our tecnologies-->
-        <section class="tf-section connect-wallet">
-            <div class="container">
-
-
-                <div class="row">
-
-
-
-                    
-                    <div class="col-lg-4 col-md-4">
-                        <div class="sc-wallet">
-                            <div class="icon">
-                                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-3.svg" alt="">
-                            </div>
-                            <div class="content">
-                                <h4><a href="#">Vue</a></h4>
-                                <p>Vue is a JavaScript frontend framework for building complex applications in a simpler and more scalable way.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4">
-                        <div class="sc-wallet">
-                            <div class="icon">
-                                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-4.svg" alt="">
-                            </div>
-                            <div class="content">
-                                <h4><a href="#">WebGL & Unity</a></h4>
-                                <p>Unity is the most popular graphic engine, this engine allows you to create very powerful graphic applications and export them to the web and other devices.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4">
-                        <div class="sc-wallet">
-                            <div class="icon">
-                                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-5.svg" alt="">
-                            </div>
-                            <div class="content">
-                                <h4><a href="#">Laravel</a></h4>
-                                <p>Laravel is a Php framework for building backends for web applications. Laravel is based on the Symphony framework.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4">
-                        <div class="sc-wallet">
-                            <div class="icon">
-                                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-6.svg" alt="">
-                            </div>
-                            <div class="content">
-                                <h4><a href="#">NodeJS</a></h4>
-                                <p>When you use nodejs you are using js but outside of the browser, nodejs is used to create server applications. In addition to js we can also create libraries with c++ so we can increase its performance.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4">
-                        <div class="sc-wallet mg-bt-0">
-                            <div class="icon">
-                                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-7.svg" alt="">
-                            </div>
-                            <div class="content">
-                                <h4><a href="#">Wordpress</a></h4>
-                                <p>Wordpress is the most popular cms, the vast majority of the world's websites use it. It has extensive documentation and tools created by third parties.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4">
-                        <div class="sc-wallet mg-bt-0">
-                            <div class="icon">
-                                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-8.svg" alt="">
-                            </div>
-                            <div class="content">
-                                <h4><a href="#">AdobeXD & Figma</a></h4>
-                                <p>Adobe xd and figma are used to create and design application sketches, I am not a designer but in this time I have had to work with this.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4">
-                        <div class="sc-wallet mg-bt-0">
-                            <div class="icon">
-                                <img src="{{env('ASSETS_URL')}}assets/images/icon/icon-9.svg" alt="">
-                            </div>
-                            <div class="content">
-                                <h4><a href="#">Electron & Ionic</a></h4>
-                                <p>Ionic builds hybrid apps compatible with IOS, Android, Windows and Web. Ionic uses web standards (html, css, JavaScript)</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
-        </section>
-        <!--End Our tecnologies-->
-        <!------------------------------------------------------------------------------------------------------------------------------------------>
-
-    </div>
 
 
 
