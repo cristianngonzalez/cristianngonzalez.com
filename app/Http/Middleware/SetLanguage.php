cec44041ac12e91eb8cffb817c659078ae368015
Cristian Gonzalez <contact@cristianngonzalez.com>
2024-02-20 13:03:33 -0300

    idioma a travez de atributo

--- app/Http/Middleware/SetLanguage.php
+++ app/Http/Middleware/SetLanguage.php
@@ -14,22 +14,32 @@ class SetLanguage
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
-    {
+    public function handle(Request $request, Closure $next){
 
-        $idiomaCliente = $request->header('Accept-Language');
-        //Tomar el primer idioma del cliente
-        $idiomaCliente = explode(',', $idiomaCliente);
-        $idiomaCliente = $idiomaCliente[0];
-
+       
         $idiomasDisponibles = ['es' , 'en'];
-        //Si el idioma esta entre la lista de disponibles
-        if(in_array($idiomaCliente , $idiomasDisponibles)){
-            //Usar el idioma del cliente
-            app()->setLocale($idiomaCliente);
+
+        //El usuario puede proveer el idioma a tavez de la url param "lang"
+        if($request->lang && in_array($request->lang, $idiomasDisponibles)){
+            
+            app()->setLocale($request->lang);
+            
         }else{
-            //Usar el idioma por defecto
-            app()->setLocale('en');
+            $idiomaCliente = $request->header('Accept-Language');
+            //Tomar el primer idioma del cliente
+            $idiomaCliente = explode(',', $idiomaCliente);
+            $idiomaCliente = $idiomaCliente[0];
+
+            
+            //Si el idioma esta entre la lista de disponibles
+            if(in_array($idiomaCliente , $idiomasDisponibles)){
+                //Usar el idioma del cliente
+                app()->setLocale($idiomaCliente);
+            }else{
+                //Usar el idioma por defecto
+                app()->setLocale('en');
+            }
+
         }
 
         return $next($request);
