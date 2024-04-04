9143828e4738cc19531c4a58a5f5b991629a032d
Cristian Gonzalez <cgonzales@pxsol.com>
2024-04-03 21:03:19 -0300

    email

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -20,7 +20,7 @@ class SubscriptionController extends Controller{
         
         try{
             $req->validate([
-                'email' => 'required|email',
+                'email' => 'required|email|max:255',
             ]);
     
             $subscription = new Subscription;
@@ -29,8 +29,9 @@ class SubscriptionController extends Controller{
             $subscription->save();
     
             //Enviamos el email
-            return $this->emailService->sendEmail($subscription->email, 'Gracias por suscribirte', 'Gracias por suscribirte a nuestro boletin', 'emails.subscription');
-    
+            $email = $this->emailService->sendEmail($subscription->email, 'Gracias por suscribirte', 'Gracias por suscribirte al la newsletter', 'emails.subscription');
+            
+
             //Enviamos con variable de session (generalmente utilizadas por el sweet alert)
             return redirect()->back()->with('status', 'success')->with('message', 'Gracias por suscribirte')->with('title', 'Gracias!');
         }catch(\Exception $e){
